f2aa22f85e19ea9246cfca7604be95a7785ab755
Ivan Kirshak <i.kirshak@gmail.com>
2020-05-23 12:26:51 +0300

    created function to show weather in printed location. i'm figuring out how to refractor the code

--- main.js
+++ main.js
@@ -15,13 +15,14 @@ function showLocation(position) {
     showWeather(URI);
 }
 
+
+
 function showWeather() {
-    fetch(URI, weatherLocationInput)
+    fetch(URI)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
             const main = document.getElementById("main");
             main.innerHTML = `
                 <h1>${data.name}</h1>
@@ -45,11 +46,41 @@ function showWeather() {
         })
 }
 
-let weatherLocationInput = document.getElementById("weatherLocationInput");
-URI = `https://api.openweathermap.org/data/2.5/weather?q=${weatherLocationInput.value}&units=metric&APPID=${API_KEY}`;
-weatherLocationBtn.addEventListener("click", showWeather(URI, weatherLocationInput.value), false);
+
+function searchWeather() {
+    let weatherLocationInput = document.getElementById("weatherLocationInput");
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${weatherLocationInput.value}&units=metric&APPID=${API_KEY}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            const main = document.getElementById("main");
+            main.innerHTML = `
+                <h1>${data.name}</h1>
+                <br>
+                <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+                <br>
+                <p class="temperature">${Number(data.main.temp).toFixed()} °C</p>
+                <p class="main-weather-description">${data.weather[0].main}</p>
+                <p class="feels-like">Feels like: ${Number(data.main.feels_like).toFixed()} °C</p>
+                <p class="humidity">Humidity ${data.main.humidity} %</p>
+                <p class="air-pressure">Air pressure ${data.main.pressure} hPa</p>
+                <p class="wind">Wind ${data.wind.speed} m/s</p>
+                <p class="clouds">Clouds: ${data.clouds.all} %</p>
+                <p class="visibility">Visibility: ${data.visibility} m</p>
+                <br>
+                <p class="general-description">Weather in ${data.name}: ${data.weather[0].description}.</p>
+            `;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+// URI = `https://api.openweathermap.org/data/2.5/weather?q=${weatherLocationInput.value}&units=metric&APPID=${API_KEY}`;
+weatherLocationBtn.addEventListener("click", searchWeather(), false);
 document.body.addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
-        showWeather(URI, weatherLocationInput.value);
+        searchWeather();
     }
 }, false)
